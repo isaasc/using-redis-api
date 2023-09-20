ca99f215c9ce5a538cb17237a858b9872d77d7a4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-20 20:18:44 -0300

    chore: removing node_modules folder

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.getAllClients = () => {
+    const time = Math.random() * 8000;
+}
+
 app.get("/", (req, res) => {
     res.status(200).send({"result": "ok"})
 })
